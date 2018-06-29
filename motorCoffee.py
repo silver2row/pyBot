5ffc7d9bf82f8b95125e244d037d405cac3b6024
Seth <silver1row@cox.net>
2018-06-28 23:18:28 -0500

    RE: Cleaning up and trying something new...
    Just making this software available and ready to work.

--- motorCoffee.py
+++ motorCoffee.py
@@ -22,44 +22,43 @@ GPIO.output(EnA, GPIO.HIGH)
  
  
 def b_callback(unused):
-        time.sleep(0.1)
-        if GPIO.input(Bt):
-                #print "start"
-                PWM.set_duty_cycle(EnA, 100) #100 only for testing
-        else:
-                #print "stop"
-                PWM.set_duty_cycle(EnA, 0)
+    time.sleep(0.1)
+    if GPIO.input(Bt):
+        #print "start"
+        PWM.set_duty_cycle(EnA, 100) #100 only for testing
+    else:
+        #print "stop"
+        PWM.set_duty_cycle(EnA, 0)
  
  
 GPIO.add_event_detect(Bt, GPIO.BOTH, callback=b_callback, bouncetime=300)
  
 def testPWM():
-        GPIO.output(ln1, GPIO.HIGH)
-        GPIO.output(ln2, GPIO.HIGH)
-        PWM.start(EnA, 0, 100)
-        for x in range(0, 100):
-                PWM.set_duty_cycle(EnA, x)
-                time.sleep(0.05)
+    GPIO.output(ln1, GPIO.HIGH)
+    GPIO.output(ln2, GPIO.HIGH)
+    PWM.start(EnA, 0, 100)
+    for x in range(0, 100):
+        PWM.set_duty_cycle(EnA, x)
+        time.sleep(0.05)
  
  
 def testH():
-        while(1):
-                motorStatus = "open"
-                GPIO.output(ln1, GPIO.HIGH)
-                GPIO.output(ln2, GPIO.LOW)
-                time.sleep(1)
+    while(1):
+        GPIO.output(ln1, GPIO.HIGH)
+        GPIO.output(ln2, GPIO.LOW)
+        time.sleep(1)
  
-                GPIO.output(ln1, GPIO.HIGH)
-                GPIO.output(ln2, GPIO.HIGH)
-                time.sleep(1)
+        GPIO.output(ln1, GPIO.HIGH)
+        GPIO.output(ln2, GPIO.HIGH)
+        time.sleep(1)
  
-                GPIO.output(ln1, GPIO.LOW)
-                GPIO.output(ln2, GPIO.HIGH)
-                time.sleep(1)
-                print ("Reverse!")
-                GPIO.output(ln1, GPIO.LOW)
-                GPIO.output(ln2, GPIO.LOW)
-                time.sleep(1)
+        GPIO.output(ln1, GPIO.LOW)
+        GPIO.output(ln2, GPIO.HIGH)
+        time.sleep(1)
+        print ("Reverse!")
+        GPIO.output(ln1, GPIO.LOW)
+        GPIO.output(ln2, GPIO.LOW)
+        time.sleep(1)
  
 testPWM()
 testH()
