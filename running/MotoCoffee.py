e75dd5ee7893e54368cdc759dd24f7e8f4a9e359
HN-Vignolles <31932468+HN-Vignolles@users.noreply.github.com>
2018-08-21 15:34:53 -0300

    Update MotoCoffee.py

--- running/MotoCoffee.py
+++ running/MotoCoffee.py
@@ -1,25 +1,24 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import time
- 
-EnA = "P8_13"
-EnB = "P8_19"
 
+EnA = "P8_13"
 I1A = "P8_9"
 I2A = "P8_10"
+
+EnB = "P8_19"
 I1B = "P8_11"
 I2B = "P8_12"
 
 Bt  = "P8_8"
- 
-GPIO.setup(EnA, GPIO.OUT)
-GPIO.setup(EnB, GPIO.OUT)
+
 GPIO.setup(I1A, GPIO.OUT)
 GPIO.setup(I2A, GPIO.OUT)
 GPIO.setup(I1B, GPIO.OUT)
 GPIO.setup(I2B, GPIO.OUT)
 
 GPIO.setup(Bt, GPIO.IN)
+
 PWM.start(EnA, 0, 100)
 PWM.start(EnB, 0, 100)
 PWM.set_duty_cycle(EnA, 0)
@@ -32,9 +31,11 @@ def b_callback(unused):
     if GPIO.input(Bt):
         #print "start"
         PWM.set_duty_cycle(EnA, 100) #100 only for testing
+        PWM.set_duty_cycle(EnB, 100)
     else:
         #print "stop"
         PWM.set_duty_cycle(EnA, 0)
+        PWM.set_duty_cycle(EnB, 0)
  
  
 GPIO.add_event_detect(Bt, GPIO.BOTH, callback=b_callback, bouncetime=300)
@@ -57,21 +58,28 @@ def testH():
     PWM.set_duty_cycle(EnA, 100)
     PWM.set_duty_cycle(EnB, 100)
     while(1):
-        GPIO.output(ln1, GPIO.HIGH)
-        GPIO.output(ln2, GPIO.LOW)
+        GPIO.output(I1A, GPIO.HIGH)
+        GPIO.output(I2A, GPIO.LOW)
+        GPIO.output(I1B, GPIO.HIGH)
+        GPIO.output(I2B, GPIO.LOW)
+        time.sleep(1)
+        
+        #Rotate
+        GPIO.output(I1B, GPIO.LOW)
+        GPIO.output(I2B, GPIO.LOW)
         time.sleep(1)
  
-        GPIO.output(ln1, GPIO.HIGH)
-        GPIO.output(ln2, GPIO.HIGH)
+        #Invert
+        GPIO.output(I1A, GPIO.LOW)
+        GPIO.output(I2A, GPIO.HIGH)
+        GPIO.output(I1B, GPIO.LOW)
+        GPIO.output(I2B, GPIO.HIGH)
         time.sleep(1)
- 
-        GPIO.output(ln1, GPIO.LOW)
-        GPIO.output(ln2, GPIO.HIGH)
-        time.sleep(1)
-        print ("Reverse!")
-        GPIO.output(ln1, GPIO.LOW)
-        GPIO.output(ln2, GPIO.LOW)
+       
+        #Rotate
+        GPIO.output(I1A, GPIO.LOW)
+        GPIO.output(I2A, GPIO.LOW)
         time.sleep(1)
  
 testPWM()
-#testH()
+testH()
