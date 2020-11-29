2eb95abbc84f2f9e007e1b2b7039dd2198a0c08f
silver2row <silver1row@cox.net>
2020-11-29 05:37:00 +0000

    making adjustments in source

--- src/MotoCoffee.py
+++ src/MotoCoffee.py
@@ -1,6 +1,9 @@
+#!/usr/bin/ python3
+
+from flask import Flask, render_template
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
-import time
+from time import sleep
 
 EnA = "P8_13"
 I1A = "P8_9"
@@ -26,8 +29,12 @@ PWM.set_duty_cycle(EnB, 0)
 
 print ("Initialize!")
 
+app = Flask(__name__)
+@app.route("/hello/")
+@app.route("/hello/<state>")
+
 def b_callback(unused):
-    time.sleep(0.1)
+    sleep(0.1)
     if GPIO.input(Bt):
         #print "start"
         PWM.set_duty_cycle(EnA, 100) #100 only for testing
@@ -40,46 +47,59 @@ def b_callback(unused):
 
 GPIO.add_event_detect(Bt, GPIO.BOTH, callback=b_callback, bouncetime=300)
 
-def testPWM():
+def testPWM(state=None):
     #Direction depends on wiring
-    GPIO.output(I1A, GPIO.HIGH)
-    GPIO.output(I2A, GPIO.LOW)
+    if state == "LEDS":
+        GPIO.output(I1A, GPIO.HIGH)
+        GPIO.output(I2A, GPIO.LOW)
 
-    GPIO.output(I1B, GPIO.HIGH)
-    GPIO.output(I2B, GPIO.LOW)
+        GPIO.output(I1B, GPIO.HIGH)
+        GPIO.output(I2B, GPIO.LOW)
 
-    for x in range(0, 100):
-        PWM.set_duty_cycle(EnA, x)
-        PWM.set_duty_cycle(EnB, x)
-        time.sleep(0.05)
+        for x in range(0, 100):
+            PWM.set_duty_cycle(EnA, x)
+            PWM.set_duty_cycle(EnB, x)
+            sleep(0.05)
 
 
-def testH():
+def testH(state=None):
     PWM.set_duty_cycle(EnA, 100)
     PWM.set_duty_cycle(EnB, 100)
-    while(1):
+    if state == "F":
+        #Forward or Reverse?
         GPIO.output(I1A, GPIO.HIGH)
         GPIO.output(I2A, GPIO.LOW)
         GPIO.output(I1B, GPIO.HIGH)
         GPIO.output(I2B, GPIO.LOW)
-        time.sleep(1)
+        sleep(1)
 
-        #Rotate
+    if state == "L":
+        #Rotate One Way!
         GPIO.output(I1B, GPIO.LOW)
         GPIO.output(I2B, GPIO.LOW)
-        time.sleep(1)
+        sleep(1)
 
-        #Invert
+    if state == "R":
+        #Invert(Forward or Reverse)?
         GPIO.output(I1A, GPIO.LOW)
         GPIO.output(I2A, GPIO.HIGH)
         GPIO.output(I1B, GPIO.LOW)
         GPIO.output(I2B, GPIO.HIGH)
-        time.sleep(1)
+        sleep(1)
 
-        #Rotate
+    if state == "Rev":
+        #Rotate the other way!
         GPIO.output(I1A, GPIO.LOW)
         GPIO.output(I2A, GPIO.LOW)
-        time.sleep(1)
+        sleep(1)
 
-testPWM()
-testH()
+    template_data = {
+        "title" : state,
+    }
+    return render_template("fortune.html", **template_data)
+
+#testPWM()
+#testH()
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- src/motorCoffee.py
+++ src/motorCoffee.py
@@ -1,6 +1,6 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
- 
+
 EnA = "P8_13"
 EnB = "P8_16"
 ln1 = "P8_14"
@@ -8,21 +8,21 @@ ln2 = "P8_15"
 ln3 = "P8_17"
 ln4 = "P8_18"
 Bt  = "P8_8"
- 
+
 GPIO.setup(EnA, GPIO.OUT)
 GPIO.setup(ln1, GPIO.OUT)
 GPIO.setup(ln2, GPIO.OUT)
 GPIO.setup(Bt, GPIO.IN)
- 
+
 GPIO.output(EnA, GPIO.LOW)
 GPIO.output(EnB, GPIO.LOW)
- 
+
 print ("Initialize!")
- 
+
 GPIO.output(EnA, GPIO.HIGH)
 GPIO.output(EnB, GPIO.HIGH)
- 
- 
+
+
 def call(unused):
     time.sleep(0.1)
     print ("Button pressed")
@@ -34,9 +34,9 @@ def call(unused):
         #print "stop"
         GPIO.output(EnA, GPIO.LOW)
         GPIO.output(EnB, GPIO.LOW)
- 
+
 GPIO.event_detected(Bt)
- 
+
 def testP():
     GPIO.output(ln1, GPIO.HIGH)
     GPIO.output(ln2, GPIO.LOW)
@@ -46,8 +46,8 @@ def testP():
         GPIO.output(EnA, GPIO.HIGH)
         GPIO.output(EnB, GPIO.HIGH)
         time.sleep(15)
- 
- 
+
+
 def testH():
     while(1):
         GPIO.output(ln1, GPIO.HIGH)
@@ -55,20 +55,20 @@ def testH():
         GPIO.output(ln3, GPIO.HIGH)
         GPIO.output(ln4, GPIO.LOW)
         time.sleep(10)
- 
+
         GPIO.output(ln1, GPIO.HIGH)
         GPIO.output(ln2, GPIO.HIGH)
         GPIO.output(ln3, GPIO.HIGH)
         GPIO.output(ln4, GPIO.HIGH)
         time.sleep(10)
- 
+
         GPIO.output(ln1, GPIO.LOW)
         GPIO.output(ln2, GPIO.HIGH)
         GPIO.output(ln3, GPIO.LOW)
         GPIO.output(ln4, GPIO.HIGH)
         time.sleep(10)
         print ("Reverse!")
-       
+
         GPIO.output(ln1, GPIO.LOW)
         GPIO.output(ln2, GPIO.LOW)
         GPIO.output(ln3, GPIO.LOW)
